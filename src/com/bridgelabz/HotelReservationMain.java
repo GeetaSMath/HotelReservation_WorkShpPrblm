e47771695320036a67058ca1d0e0837d012dc15c
GeetaSMath <geetasmath@gmail.com>
2021-12-02 21:50:41 +0530

    Added: ability to add hotel reservation system by person name and rate

--- src/com/bridgelabz/HotelReservationMain.java
+++ src/com/bridgelabz/HotelReservationMain.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class HotelReservationMain {
     public static void main(String[] args) {
         System.out.println("Welcome to solve hotel Reservation System");
+
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.viewHotels();
     }
 }
