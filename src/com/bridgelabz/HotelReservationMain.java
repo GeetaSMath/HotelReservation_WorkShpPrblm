63acdfe045109e3a94619de491519b6ca914debc
GeetaSMath <geetasmath@gmail.com>
2021-12-04 14:33:19 +0530

    Added:cheapest hotel range based on weekend rate

--- src/com/bridgelabz/HotelReservationMain.java
+++ src/com/bridgelabz/HotelReservationMain.java
@@ -1,13 +1,15 @@
 package com.bridgelabz;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-public class HotelReservationMain {
+class HotelReservationSystemMain{
     public static void main(String[] args) {
-        System.out.println("Welcome to solve hotel Reservation System");
-
         HotelReservation hotelReservation = new HotelReservation();
         hotelReservation.addHotel("LakeWood", 110, 90);
         hotelReservation.addHotel("BridgeWood", 150, 50);
         hotelReservation.addHotel("RidgeWood", 220, 150);
         hotelReservation.getHotelDetails();
+
+
     }
 }
