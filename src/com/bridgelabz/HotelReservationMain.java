f1de95799f1689e554bd77161a675efdaab7c32d
GeetaSMath <geetasmath@gmail.com>
2021-12-04 14:20:05 +0530

    Added: ability to add weekend rate

--- src/com/bridgelabz/HotelReservationMain.java
+++ src/com/bridgelabz/HotelReservationMain.java
@@ -5,7 +5,9 @@ public class HotelReservationMain {
         System.out.println("Welcome to solve hotel Reservation System");
 
         HotelReservation hotelReservation = new HotelReservation();
-        hotelReservation.viewHotels();
-        hotelReservation.getMinPriceHotel();
+        hotelReservation.addHotel("LakeWood", 110, 90);
+        hotelReservation.addHotel("BridgeWood", 150, 50);
+        hotelReservation.addHotel("RidgeWood", 220, 150);
+        hotelReservation.getHotelDetails();
     }
 }
